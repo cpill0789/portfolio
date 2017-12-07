9abdd480a901a588b6b95c2a158777dd370104fd
Carl Pillot <carl.pillot@gmail.com>
2017-12-07 15:10:04 -0800

    fix: Toggle site mask by tap/click instead of hover.

--- src/portfolio/components/SiteScreen.js
+++ src/portfolio/components/SiteScreen.js
@@ -4,11 +4,27 @@ import PropTypes from 'prop-types';
 import './SiteScreen.css';
 
 class SiteScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mask: true,
+    };
+
+    this.toggleMask = this.toggleMask.bind(this);
+  }
+
+  toggleMask() {
+    this.setState(prevState => ({
+      mask: !prevState.mask,
+    }));
+  }
+
   render() {
     return (
-      <div className="SiteScreen">
+      <div className="SiteScreen" onClick={this.toggleMask}>
         <img src={this.props.image} alt="" className="SiteScreen-image" />
-        { this.props.mask && <img src={this.props.mask} alt="" className="SiteScreen-mask" /> }
+        { this.props.mask && <img src={this.props.mask} alt="" className={`SiteScreen-mask ${this.state.mask ? ' show' : ''}`} /> }
       </div>
     );
   }
