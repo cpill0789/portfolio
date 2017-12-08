ea0ebda971a46bac8ff77007a2a96d8c6bc8a6d9
Carl Pillot <carl.pillot@gmail.com>
2017-12-08 12:54:01 -0800

    fix: Update video behavior to show overlay when paused and seek to beginning on ended event.

--- src/portfolio/components/PhoneFrame.js
+++ src/portfolio/components/PhoneFrame.js
@@ -8,36 +8,45 @@ class PhoneFrame extends PureComponent {
   constructor(props) {
     super(props);
     this.bindVideoRef = this.bindVideoRef.bind(this);
-    this.playVideo = this.playVideo.bind(this);
+    this.toggleVideo = this.toggleVideo.bind(this);
+    this.handleVideoEnded = this.handleVideoEnded.bind(this);
+
     this.state = {
-      clicked: false,
+      showOverlay: true,
     };
   }
 
-  getOverlay() {
-    if (!this.state.clicked) {
-      return (
-        <div className="PhoneFrame-overlay" onClick={this.playVideo}>
-          <svg viewBox="0 0 1792 1792" width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M896 128q209 0 385.5 103T1561 510.5 1664 896t-103 385.5-279.5 279.5T896 1664t-385.5-103T231 1281.5 128 896t103-385.5T510.5 231 896 128zm384 823q32-18 32-55t-32-55L736 521q-31-19-64-1-32 19-32 56v640q0 37 32 56 16 8 32 8 17 0 32-9z" fill="#CCC" /></svg>
-        </div>
-      );
-    }
-
-    return null;
+  componentWillUnmount() {
+    this.video.removeEventListener('ended');
   }
 
-  playVideo() {
-    this.setState({ clicked: true });
+  getOverlay() {
+    const className = `PhoneFrame-overlay${this.state.showOverlay ? '' : ' hidden'}`;
+    return (
+      <div className={className} onClick={this.toggleVideo}>
+        <svg viewBox="0 0 1792 1792" width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M896 128q209 0 385.5 103T1561 510.5 1664 896t-103 385.5-279.5 279.5T896 1664t-385.5-103T231 1281.5 128 896t103-385.5T510.5 231 896 128zm384 823q32-18 32-55t-32-55L736 521q-31-19-64-1-32 19-32 56v640q0 37 32 56 16 8 32 8 17 0 32-9z" fill="#CCC" /></svg>
+      </div>
+    );
+  }
 
+  toggleVideo() {
     if (this.video.paused) {
       this.video.play();
+      this.setState(() => ({ showOverlay: false }));
     } else {
       this.video.pause();
+      this.setState(() => ({ showOverlay: true }));
     }
+  }
+
+  handleVideoEnded() {
+    this.setState(() => ({ showOverlay: true }));
+    this.video.currentTime = 0;
   }
 
   bindVideoRef(video) {
     this.video = video;
+    video.addEventListener('ended', this.handleVideoEnded);
   }
 
   render() {
@@ -47,7 +56,6 @@ class PhoneFrame extends PureComponent {
         { this.getOverlay() }
         <video
           ref={this.bindVideoRef}
-          onClick={this.playVideo}
           className="PhoneFrame-video"
           poster={this.props.poster}
         >
